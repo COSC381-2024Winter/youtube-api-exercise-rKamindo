5c06d7dbd736fabb677cf9cf28b69330f2223b13
rKAmindo <randykamindo@gmail.com>
2024-03-19 12:29:25 -0400

    enhancement: Add ability to search youtube API
    The following changes were made:
    1. Modified youtube.py to allow the user to search for videos by name, as well as limit the search with maxResults
    2. Exported venv dependencies to requirements.txt
    
    issue #3

--- youtube.py
+++ youtube.py
@@ -1,3 +1,30 @@
+import argparse
+import sys
 import config
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
-print(config.API_KEY)
+DEVELOPER_KEY =  config.API_KEY
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
+
+def youtube_search(query_term, max_results):
+    youtube = build(YOUTUBE_API_SERVICE_NAME,
+                    YOUTUBE_API_VERSION,
+                    developerKey=DEVELOPER_KEY)
+    
+    # call the search.list method to retrieve results matching the specified query term
+    search_response = youtube.search().list(
+        q=query_term,
+        part='id, snippet',
+        maxResults=max_results
+    ).execute()
+
+    return search_response
+
+if __name__ == "__main__":
+    query_term = sys.argv[1]
+    max_results = sys.argv[2]
+    response = youtube_search(query_term, max_results)
+    print(response)
+    print(sys.prefix)
